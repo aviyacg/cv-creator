b25bf4516d8b7e33d0fe0f1f2186263daf283933
Aviya CG <aviyacohen.tchvfvi@gmail.com>
2022-11-29 20:09:18 +0200

    Create three sections and thier inputList

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,10 +5,32 @@ class Form extends Component {
     const infoInputlist = [
       { placeHolder: "First name" },
       { placeHolder: "Last name" },
+      { placeHolder: "Title" },
+      { placeHolder: "Address" },
       { placeHolder: "Phone number", type: "tel" },
+      { placeHolder: "Email", type: "email" },
+    ];
+    const expInputList = [
+      { placeHolder: "Position" },
+      { placeHolder: "Company" },
+      { placeHolder: "City" },
+      { placeHolder: "From year", type: "number" },
+      { placeHolder: "Until year", type: "number" }
+    ];
+    const eduInputList = [
+      { placeHolder: "Istitution" },
+      { placeHolder: "City" },
+      { placeHolder: "Degree" },
+      { placeHolder: "Subject" },
+      { placeHolder: "From year", type: "number" },
+      { placeHolder: "Until year", type: "number" }
     ];
     return (
-      <Section title="Personal Info" inputList={infoInputlist}/>
+      <>
+        <Section title="Personal Info" inputList={infoInputlist} />
+        <Section title="Experience" inputList={expInputList} />
+        <Section title="Education" inputList={eduInputList} />
+      </>
     );
   }
 }
