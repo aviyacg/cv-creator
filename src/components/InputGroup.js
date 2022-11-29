c2f95eef7e536b57cd397428fb7277cabbf2a178
Aviya CG <aviyacohen.tchvfvi@gmail.com>
2022-11-29 20:12:14 +0200

    Implement input keys based on index

--- src/components/InputGroup.js
+++ src/components/InputGroup.js
@@ -3,9 +3,9 @@ import { Component } from "react";
 class InputGroup extends Component {
   render() {
     const { inputList } = this.props;
-    const liList = inputList.map(input => {
+    const liList = inputList.map((input, index) => {
       const { placeHolder, type } = input;
-      return (<input type={type} placeholder={placeHolder} />);
+      return (<input key={index} type={type} placeholder={placeHolder} />);
     });
     return (
       <div className="InputGroup">
