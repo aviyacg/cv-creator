d32018d1b60dbb5b58cd92d25097c2d110a05dc6
Aviya CG <aviyacohen.tchvfvi@gmail.com>
2022-11-29 12:38:11 +0200

    Create Header component

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import { Component } from "react";
+import '../styles/Header.css';
+
+class Header extends Component{
+  render(){
+    return (
+      <div className="Header">
+        Create CV
+      </div>
+    );
+  }
+}
+
+export default Header;
