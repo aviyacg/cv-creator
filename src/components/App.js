f77cd31f64b00e6986dedb1a5f0f5867a24fabf7
Aviya CG <aviyacohen.tchvfvi@gmail.com>
2022-11-30 18:46:57 +0200

    Write constructor with intial state structor

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,38 @@ import '../styles/App.css';
 import Header from './Header';
 import Form from './Form';
 import { Component } from 'react';
+import { v4 as newId } from 'uuid';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      personal: [
+        {
+          id: newId(),
+          info: {},
+        },
+      ],
+      experience: [
+        {
+          id: newId(),
+          info: {},
+        },
+      ],
+      education: [
+        {
+          id: newId(),
+          info: {},
+        },
+      ],
+    };
+  }
+
+  changeInfo() {
+
+  }
+
   render() {
     return (
       <div className="App">
