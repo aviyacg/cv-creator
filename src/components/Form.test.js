f328193a2d3bba1106f211cfeec0e87a6538ab37
Aviya CG <aviyacohen.tchvfvi@gmail.com>
2022-11-30 22:12:10 +0200

    Write Form render tests

--- src/components/Form.test.js
+++ src/components/Form.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Form from './Form';
 
 test('renders Three sections titles', () => {
@@ -13,4 +14,81 @@ test('renders Three sections titles', () => {
   expect(eduTitle).not.toBeUndefined();
 });
 
+test('renders two add buttons', () => { 
+  render(<Form />);
+  const addButtons = screen.getAllByText('Add');
+  expect(addButtons).toHaveLength(2);
+});
+
+test('renders two delete buttons', () => { 
+  render(<Form />);
+  const deleteButtons = screen.getAllByText('Delete');
+  expect(deleteButtons).toHaveLength(2);
+});
+
+test('renders one experience inputGroup', () => { 
+  render(<Form />);
+  const positionInputs = screen.getAllByPlaceholderText('Position');
+  expect(positionInputs).toHaveLength(1);
+
+  const companyInputs = screen.getAllByPlaceholderText('Company');
+  expect(companyInputs).toHaveLength(1);
+});
+
+test('renders one education inputGroup', () => { 
+  render(<Form />);
+  const institutionInputs = screen.getAllByPlaceholderText('Institution');
+  expect(institutionInputs).toHaveLength(1);
+
+  const degreeInputs = screen.getAllByPlaceholderText('Degree');
+  expect(degreeInputs).toHaveLength(1);
+});
+
+test('add button add experience inputGroup', () => {
+  render(<Form />);
+  const positionInputs1 = screen.getAllByPlaceholderText('Position');
+  expect(positionInputs1).toHaveLength(1);
+
+  const [addExp, ] = screen.getAllByText('Add');
+  userEvent.click(addExp);
+
+  const positionInputs2 = screen.getAllByPlaceholderText('Position');
+  expect(positionInputs2).toHaveLength(2);
+});
+
+test('add button add education inputGroup', () => {
+  render(<Form />);
+  const institutionInputs1 = screen.getAllByPlaceholderText('Institution');
+  expect(institutionInputs1).toHaveLength(1);
+
+  const [, addEdu] = screen.getAllByText('Add');
+  userEvent.click(addEdu);
+
+  const institutionInputs2 = screen.getAllByPlaceholderText('Institution');
+  expect(institutionInputs2).toHaveLength(2);
+});
+
+test('delete button delete experience inputGroup', () => {
+  render(<Form />);
+  const positionInputs1 = screen.getAllByPlaceholderText('Position');
+  expect(positionInputs1).toHaveLength(1);
+
+  const [deleteExp, ] = screen.getAllByText('Delete');
+  userEvent.click(deleteExp);
+
+  const positionInputs2 = screen.queryAllByPlaceholderText('Position');
+  expect(positionInputs2).toHaveLength(0);
+});
+
+test('delete button delete education inputGroup', () => {
+  render(<Form />);
+  const institutionInputs1 = screen.getAllByPlaceholderText('Institution');
+  expect(institutionInputs1).toHaveLength(1);
+
+  const [, deleteEdu] = screen.getAllByText('Delete');
+  userEvent.click(deleteEdu);
+
+  const institutionInputs2 = screen.queryAllByPlaceholderText('Institution');
+  expect(institutionInputs2).toHaveLength(0);
+});
 
