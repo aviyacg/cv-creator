dd2f3cb5545591c6bce4c6e6701b7bdf38907a89
Aviya CG <aviyacohen.tchvfvi@gmail.com>
2022-12-05 15:24:01 +0200

    Add export pdf button to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,21 @@
 import { Component } from "react";
+import JsPDF from 'jspdf';
 import '../styles/Header.css';
 
 class Header extends Component{
+
+  exportPDF() {
+    const pdf = new JsPDF('portrait','pt','a4');
+    pdf.html(document.querySelector('.Preview')).then(() => {
+      pdf.save('CV.pdf');
+    });
+  }
+
   render(){
     return (
       <div className="Header">
         Create CV
+        <button onClick={this.exportPDF}>Export PDF</button>
       </div>
     );
   }
